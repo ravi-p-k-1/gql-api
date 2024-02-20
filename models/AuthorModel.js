8417bee67505b568118ed1cfa621e961fe228104
ravikth <ravi.kakadia@techholding.co>
2024-02-20 18:33:55 +0530

    changed sequelize models from define to class models

--- models/AuthorModel.js
+++ models/AuthorModel.js
@@ -1,7 +1,28 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../db.js";
 
-export const Author = sequelize.define("Author",{
+// export const Author = sequelize.define("Author",{
+//     id: {
+//         type: DataTypes.UUID, 
+//         defaultValue: DataTypes.UUIDV4,
+//         primaryKey: true,
+//         allowNull: false,
+//     },
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     age: {
+//         type: DataTypes.INTEGER,
+//         allowNull: false,
+//     }
+// },{
+//     freezeTableName: true,
+// });
+
+export class Author extends Model {}
+
+Author. init({
     id: {
         type: DataTypes.UUID, 
         defaultValue: DataTypes.UUIDV4,
@@ -17,6 +38,8 @@ export const Author = sequelize.define("Author",{
         allowNull: false,
     }
 },{
+    sequelize: sequelize,
+    modelName: "Author",
     freezeTableName: true,
-});
+})
 
--- models/BookModel.js
+++ models/BookModel.js
@@ -1,7 +1,28 @@
-import { DataTypes } from "sequelize";
+import { DataTypes, Model } from "sequelize";
 import { sequelize } from "../db.js";
 
-export const Book = sequelize.define("Book",{
+// export const Book = sequelize.define("Book",{
+//     id: {
+//         type: DataTypes.UUID, 
+//         defaultValue: DataTypes.UUIDV4,
+//         primaryKey: true,
+//         allowNull: false,
+//     },
+//     name: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     },
+//     genre: {
+//         type: DataTypes.STRING,
+//         allowNull: false,
+//     }
+// },{
+//     freezeTableName: true,
+// });
+
+export class Book extends Model{}
+
+Book.init({
     id: {
         type: DataTypes.UUID, 
         defaultValue: DataTypes.UUIDV4,
@@ -17,6 +38,7 @@ export const Book = sequelize.define("Book",{
         allowNull: false,
     }
 },{
+    sequelize: sequelize,
+    modelName: "Book",
     freezeTableName: true,
-});
-
+})
